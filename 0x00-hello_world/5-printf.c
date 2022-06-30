7af6a2992f3f9b0347b3ead836fc3220b07cef8c
Tron987 <tronkazembe@mail.com>
2022-06-30 05:12:20 -0700

    ehh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints quote
- *   *
- *    * Return: zero on success
- *     *
- *      */
+ * main - Prints quo
+ *
+ * Return: zero on success
+ *
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
